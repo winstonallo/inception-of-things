7d796d23d67281fdea0ea3861ae9debc3e20d5e7
winstonallo <arthurbiedchar@gmail.com>
2025-01-05 15:08:49 +0100

    fix: add --flannel-iface to display the correct internal ip in 'kubectl get nodes -o wide'

--- p1/abied-chS.sh
+++ p1/abied-chS.sh
@@ -4,11 +4,12 @@ export LANG=en_US
 export LANGUAGE=en
 
 # Install K3s
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" K3S_NODE_IP=192.168.56.110 sh -
 
 mkdir -p /home/vagrant/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-chown -R vagrant:vagrant /home/vagrant/.kube/config 
+chown -R vagrant:vagrant /home/vagrant/.kube/config
+chmod 770 /etc/rancher/k3s
 
 TOKEN=$(cat /var/lib/rancher/k3s/server/node-token)
 
--- p1/abied-chSW.sh
+++ p1/abied-chSW.sh
@@ -7,5 +7,5 @@ MASTER_IP=$1
 
 TOKEN=$(cat /vagrant/token)
 
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
 
